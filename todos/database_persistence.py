43925c0e9b5f544f010cf822b1c266e90b4c6b0b
Robert Tsui <rt.roberttsui@gmail.com>
2025-04-03 14:19:37 -0700

    Added logic for displaying all todo lists, a single list, logging, loading todos, creating a new list, and updating a list

--- todos/database_persistence.py
+++ todos/database_persistence.py
@@ -1,7 +1,12 @@
 from contextlib import contextmanager 
 
+import logging
 import psycopg2
-from psycopg2.extras import DictCursor
+from psycopg2.extras import DictCursor 
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
 
 class DatabasePersistence:
     def __init__(self):
@@ -17,16 +22,43 @@ class DatabasePersistence:
             connection.close()
     
     def find_list(self, list_id):
-        pass
+        query = 'SELECT * FROM lists WHERE id = %s'
+        logger.info("Executing query: %s with list_id: %s", query, list_id)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (list_id,))
+                lst = dict(cursor.fetchone())
+        lst.setdefault('todos', [])
+        lst['todos'] = lst['todos'] + self._find_todos_for_list(list_id)
+        return lst  
     
     def all_lists(self):
-        pass
-    
+        query = 'SELECT * FROM lists'
+        logger.info("Executing query: %s", query)
+        with self._database_connect() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+        lists = [dict(result) for result in results]
+        for lst in lists:
+            todos = self._find_todos_for_list(lst['id'])
+            lst.setdefault('todos', todos)
+
+        return lists
+
     def create_new_list(self, title):
-        pass
+        query = "INSERT INTO lists (title) VALUES (%s)"
+        logger.info("Executing query: %s with title: %s", query, title)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (title,))
 
     def update_list_by_id(self, list_id, new_title):
-        pass
+        query = "UPDATE lists SET title = %s WHERE id = %s"
+        logger.info("Executing query: %s with new_title: %s and id: %s", query, new_title, list_id)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (new_title, list_id))
     
     def delete_list(self, list_id):
         pass
@@ -42,3 +74,11 @@ class DatabasePersistence:
     
     def mark_all_todos_completed(self, list_id):
         pass
+
+    def _find_todos_for_list(self, list_id):
+        query = 'SELECT * FROM todos WHERE list_id = %s'
+        logger.info('Execute query: %s with list_id: %s', query, list_id)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (list_id,))
+                return cursor.fetchall()
